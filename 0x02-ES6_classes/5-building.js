4251761c5d9b11799c09dd45aba3f6e470b074cf
Kingsley Ntow Dade <139249232+Kingsley97Dade@users.noreply.github.com>
2024-10-05 14:05:46 +0000

    Update 5-building.js

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,26 +1,14 @@
-class Building {
-    constructor(sqft) {
-      this._sqft = this.validateNumber(sqft, 'Square Feet');
-    }
-  
-    // Getter for sqft
-    get sqft() {
-      return this._sqft;
-    }
-  
-    // Abstract method to be implemented by subclasses
-    evacuationWarningMessage() {
+export default class Building {
+  constructor(sqft = 0) {
+    if (this.constructor !== Building
+        && typeof this.evacuationWarningMessage !== 'function') {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
-  
-    // Helper function to validate number type
-    validateNumber(value, attributeName) {
-      if (typeof value !== 'number' || isNaN(value)) {
-        throw new TypeError(`${attributeName} must be a number`);
-      }
-      return value;
-    }
+
+    this._sqft = sqft;
   }
-  
-  export default Building;
-  
+
+  get sqft() {
+    return this._sqft;
+  }
+}

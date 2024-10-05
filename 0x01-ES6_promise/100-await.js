fe1c274484342f29a1c4390592c14d57dff8f187
Kingsley Ntow Dade <139249232+Kingsley97Dade@users.noreply.github.com>
2024-10-05 13:49:47 +0000

    Update 100-await.js

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,11 +1,17 @@
-import { uploadPhoto, createUser } from "./utils.js";
+import { uploadPhoto, createUser } from './utils';
+
 export default async function asyncUploadUser() {
   try {
     const photo = await uploadPhoto();
     const user = await createUser();
 
-    return { user, photo };
-  } catch (e) {
-    return { photo: null, user: null };
+    return Promise.resolve({
+      photo, user,
+    });
+  } catch (err) {
+    return Promise.resolve({
+      photo: null,
+      user: null,
+    });
   }
 }

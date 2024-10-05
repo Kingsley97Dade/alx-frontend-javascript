32a5a2053ed561e0d221f6740630db3af7022636
Kingsley Ntow Dade <139249232+Kingsley97Dade@users.noreply.github.com>
2024-10-05 13:36:52 +0000

    Update 100-createIteratorObject.js

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,32 +1,12 @@
 export default function createIteratorObject(report) {
-    const iterator = {
-      departments: Object.keys(report.allEmployees),
-      currentDepartmentIndex: 0,
-      currentEmployeeIndex: 0,
-      next() {
-        if (this.currentDepartmentIndex >= this.departments.length) {
-          return { done: true };
-        }
-  
-        const department = this.departments[this.currentDepartmentIndex];
-        const employees = report.allEmployees[department];
-  
-        if (this.currentEmployeeIndex >= employees.length) {
-          this.currentDepartmentIndex++;
-          this.currentEmployeeIndex = 0;
-          return this.next();
-        }
-  
-        const employee = employees[this.currentEmployeeIndex];
-        this.currentEmployeeIndex++;
-  
-        return { value: employee, done: false };
-      },
-    };
-  
-    iterator[Symbol.iterator] = function () {
-      return this;
-    };
-  
-    return iterator;
+  const emps = [];
+  // eslint-disable-next-line
+  for (const dep of Object.keys(report.allEmployees)) {
+    // eslint-disable-next-line
+    for (const emp of report.allEmployees[dep]) {
+      emps.push(emp);
+    }
   }
+
+  return emps;
+}

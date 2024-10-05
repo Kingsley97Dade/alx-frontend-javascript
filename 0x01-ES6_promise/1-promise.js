c3130d7117e459d2bbbf42e983fa16496b959442
Kingsley Ntow Dade <139249232+Kingsley97Dade@users.noreply.github.com>
2024-10-05 13:49:14 +0000

    Update 1-promise.js

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,7 +1,13 @@
-export default function getFullResponseFromAPI(success){
-    const promise = new promise((resolve, reject) => {
-        if(success) resolve({status: 200, body: 'Success'})
-        else reject(new Error("The fake API is not working currently"))
-    });
-    return promise;
+/* eslint-disable */
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success === true) {
+      resolve({
+        status: 200,
+        body: "Success",
+      });
+    } else {
+      reject(new Error("The fake API is not working currently"));
+    }
+  });
 }
